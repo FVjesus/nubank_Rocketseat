bbf9b4438698f1b32f9264eed0bc58cce5eb4327
Fabrício <fabriciovellozo@gmail.com>
2020-02-15 23:09:02 -0300

    Add the main card
    adds the main card and its styling

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import { Container } from './styles';
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Descripition, Annotation } from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+            <Card>
+                <CardHeader>
+                    <Icon name="attach-money" size={28} color="#666" />
+                    <Icon name="visibility-off" size={28} color="#666" />
+                </CardHeader>
+                <CardContent>
+                    <Title>Saldo disponivel</Title>
+                    <Descripition>R$167.611,25 </Descripition>
+                </CardContent>
+                <CardFooter>
+                    <Annotation>
+                        Transferência de R$ 200,00 recebida de Fabio Jesus hoje ás 07:00h
+                    </Annotation>
+                </CardFooter>
+            </Card>
+        </Content>
+
       <Tabs />
     </Container>
   );
